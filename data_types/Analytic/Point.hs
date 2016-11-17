ae6d0e85ac328a57213c536580d74941b30a8582
thalesaguiar21 <thalesaguiar21@gmail.com>
2016-11-17 16:00:33 -0300

    Melhor implementação de Point - Vector2 problemas para importar modulos

---
+++ data_types/Analytic/Point.hs
@@ -0,0 +1,25 @@
+module Analytic.Point
+( Point2 (..)
+, Point3 (..)
+) where
+
+
+data Point2 a  = Point2 (a, a) deriving (Eq, Show)
+
+instance (Num x) => Num (Point2 x) where
+	Point2 (x, y) + Point2 (w, z) = Point2 (x+w, y+z)
+	Point2 (x, y) - Point2 (w, z) = Point2 (x-w, y-z)
+	Point2 (x, y) * Point2 (w, z) = Point2 (x*w, y*z)
+	abs    (Point2 (x,y)) = Point2 (abs x,    abs y) 
+	signum (Point2 (x,y)) = Point2 (signum x, signum y)
+	fromInteger i = Point2 (fromInteger i, fromInteger i)
+
+data Point3 a = Point3 (a, a, a) deriving (Eq, Show)
+
+instance (Num x) => Num (Point3 x) where
+	Point3 (x, y, z) + Point3 (i, j, k) = Point3 (x+i, y+j, z+k)
+	Point3 (x, y, z) - Point3 (i, j, k) = Point3 (x-i, y-j, z-k)
+	Point3 (x, y, z) * Point3 (i, j, k) = Point3 (x*i, y*i, z*k)
+	abs    (Point3 (x, y, z)) = Point3 (abs x, abs y, abs z) 
+	signum (Point3 (x, y, z)) = Point3 (signum x, signum y, signum z) 
+	fromInteger i = Point3 (fromInteger i, fromInteger i, fromInteger i)
---
+++ data_types/Analytic/Vector2.hs
@@ -0,0 +1,7 @@
+module Analytic.Vector2
+( Vector2 (..)
+) where
+
+import Analytic.Point
+
+data Vector2 = Vector2 Point2 Point2
