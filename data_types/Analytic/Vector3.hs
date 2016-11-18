c26f01b7d9da96d70dd46a4c4c7233d078f20307
thalesaguiar21 <thalesaguiar21@gmail.com>
2016-11-18 02:28:46 -0300

    > Produto vetorial implementado

--- data_types/Analytic/Vector3.hs
+++ data_types/Analytic/Vector3.hs
@@ -1,35 +1,52 @@
 data Vector3 = Vector3 (Float, Float, Float, Float, Float, Float)
-	deriving (Eq, Show)
+    deriving (Eq, Show)
 
 
-
+-- Adição entre os vetores no espaço
 (|+|) :: Vector3 -> Vector3 -> Vector3
 Vector3 ( i, j, k, l, m, n ) |+| Vector3 ( o, p, q, r, s, t ) = Vector3 (i+o, j+p, k+q, l+r, m+s, n+t)
 
+-- Subtração entre vetores no espaço
 (|-|) :: Vector3 -> Vector3 -> Vector3
 Vector3 ( i, j, k, l, m, n ) |-| Vector3 ( o, p, q, r, s, t ) = Vector3 (i-o, j-p, k-q, l-r, m-s, n-t)
 
+-- Produto entre os vetores no espaço
 (|*|) :: Vector3 -> Vector3 -> Vector3
 Vector3 ( i, j, k, l, m, n ) |*| Vector3 ( o, p, q, r, s, t ) = Vector3 (i*o, j*p, k*q, l*r, m*s, n*t)
 
+-- Vetor equivalente com início na origem
 origin :: Vector3 -> (Float, Float, Float)
 origin (Vector3 (i, j, k, l, m, n)) = (l-i, m-j, n-k)
 
+-- Ponto inicial do vetor no espaço
 start :: Vector3 -> (Float, Float, Float)
 start (Vector3 (i, j, k, _, _, _)) = (i, j, k)
 
+-- Ponto final do vetor no espaço
 end :: Vector3 -> (Float, Float, Float)
 end (Vector3 (_, _, _, l, m, n)) = (l, m, n)
 
+-- Norma de um vetor no espaço
 norm :: Vector3 -> Float
 norm v = let (x, y, z) = origin v
          in sqrt (x^2 + y^2 + z^2)
 
+-- Ângulo entre dois vetores no espaço
+angle :: Vector3 -> Vector3 -> Float
+angle v1 v2 = let dprod = v1 `dotProd` v2
+              in acos dprod
+
+-- Produto interno
 dotProd :: Vector3 -> Vector3 -> Float
 v1 `dotProd` v2 = let (x1, y1, z1) = origin v1
                       (x2, y2, z2) = origin v2
                   in x1*x2 + y1*y2 + z1*z2
 
-angle :: Vector3 -> Vector3 -> Float
-angle v1 v2 = let dprod = v1 `dotProd` v2
-              in acos dprod
+-- Produto vetorial
+(<>) :: Vector3 -> Vector3 -> Vector3
+v1 <> v2 = let (x1, y1, z1) = origin v1
+               (x2, y2, z2) = origin v2
+               i = y1*z2 - z1*y2
+               j = x2*z1 - x1*z2
+               k = x1*y2 - y1*x2
+           in Vector3 (0, 0, 0, i, j, k)
