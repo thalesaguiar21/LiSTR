1514101a2499fdd5cb5f8dab037bab243d0fa154
thalesaguiar21 <thalesaguiar21@gmail.com>
2016-11-19 23:17:07 -0300

    Operações básicas de básicas com sets

--- data_types/Set/Set.hs
+++ data_types/Set/Set.hs
@@ -1,11 +1,25 @@
+infixr 5 :>
+data Set a = Empty | a :> (Set a) deriving (Read)
 
+instance Show a => Show (Set a) where
+  show Empty = "-"
+  show (x :> Empty) = show x
+  show (x :> rest) = show x ++ " " ++ show rest
 
-data Set a = Empty
-    | Put a (Set a)
-    deriving (Show, Read)
+isEmpty :: Eq a => Set a -> Bool
+isEmpty Empty = True
+isEmpty x     = False
 
+in' :: Eq a => Set a -> a -> Bool
+in' Empty _        = False
+in' (x :> rest) y  = if x == y then True
+                     else in' rest y
 
-find :: Eq a => Set a -> a -> Bool
-find Empty _ = False
-find (Put x (residue)) y = if x == y then True
-                         else find residue y
+rm :: Eq a => Set a -> a -> Set a
+rm  Empty _ = Empty
+rm (x :> rest) y = if x == y then rest
+                   else x :> rm rest y
+
+size :: Eq a => Set a -> Int
+size Empty = 0
+size (x :> rest) = 1 + size rest
