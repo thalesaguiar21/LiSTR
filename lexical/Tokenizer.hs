db6e326782a50bbe1e2729419b3581099b698250
thalesaguiar21 <thalesaguiar21@gmail.com>
2016-11-26 13:44:01 -0300

    Remoção do Tokenizer

--- lexical/Tokenizer.hs
+++
@@ -1,8 +0,0 @@
-tokenizer :: String -> [String]
-tokenizer [] = "E":[]
-tokenizer (h:t) = if h `elem` listaDeliminatores then (h:[]):tokenizer t
-                      else let ((h2:h1):t1) = tokenizer t
-                           in
-                        if h2 `elem` listaDeliminatores then (h:[]):(h2:h1):(t1)
-                            else (h:(h2:h1)):t1
-                where listaDeliminatores = ['+','-','(',')']
